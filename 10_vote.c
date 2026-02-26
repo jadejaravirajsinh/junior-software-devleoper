16a5bb322050c77ab08e78994ebf9b2b06441309
jadejaravirajsinh <ravirajj911@gmail.com>
2026-02-26 19:12:21 +0530

    Add files via upload

---
+++ 10_vote.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int age;
+
+    printf("enter your age:");
+    scanf("%d",&age);
+    if(age>=18)
+    {
+        printf("your eligable for vote.");
+    }
+        else
+        {
+    printf("your are not eligable for vote,kale aavo");
+        }
+}
